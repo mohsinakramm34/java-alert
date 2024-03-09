df215b4dae3e9a202c7ebde1c1dc5122ab38c3fa
Muhammad Mohsin Shah <160424256+mohsinakramm34@users.noreply.github.com>
2024-03-09 12:07:58 +0500

    Add files via upload
    Java alerts

---
+++ app.js
@@ -0,0 +1,23 @@
+// Assiment in javaScript
+// Muhammad Mohsin shah 
+// First Chapter in Alert
+
+//alert(" Error ! Please Enter  a valid Password.")
+//alert("Wellcome to JS Land... \n Happy Coding!")
+//alert("Wellcome to JS Land...")
+//alert(" Happy Coding ! \n Prevent this page for Creating additional dialogs..")
+// alert (" Hello ... I can run JS through my web browser's console ")
+// console.log(alert)
+
+// Second Chapter variabel string 
+var firstName = "Muhammad Mohsin Shah "
+alert(firstName)
+var age = "20 years old";
+alert(age)
+var certiFied = " Certified Mobile Application Development"
+alert(certiFied)
+var lastName = " Pizza \n Pizz \n Piz \n Pi \n P"
+alert(lastName)
+var email = " My Email address is example @muhammadmohsinakram95@gmail.com"
+alert(email)
+// third Chapter Variable Num
